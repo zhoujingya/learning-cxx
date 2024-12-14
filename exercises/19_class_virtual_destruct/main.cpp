9cc724ce0b9409956300802d7fc9a7d25df95195
zhoujingya <jing.zhou@terapines.com>
2024-12-14 22:48:23 +0800

    Update

--- exercises/19_class_virtual_destruct/main.cpp
+++ exercises/19_class_virtual_destruct/main.cpp
@@ -5,27 +5,32 @@
 
 struct A {
     // TODO: 正确初始化静态字段
-    static int num_a = 0;
+    static int num_a;
 
     A() {
         ++num_a;
+        std::cout << "A::num_a: " << num_a << std::endl;
     }
-    ~A() {
+    // Make destructor virtual to allow proper cleanup of derived classes
+    virtual ~A() {
         --num_a;
+        std::cout << "A::~A: " << num_a << std::endl;
     }
 
     virtual char name() const {
         return 'A';
     }
 };
+
+int A::num_a = 0;
 struct B final : public A {
     // TODO: 正确初始化静态字段
-    static int num_b = 0;
+    static int num_b;
 
     B() {
         ++num_b;
     }
-    ~B() {
+    ~B() override {
         --num_b;
     }
 
@@ -33,14 +38,18 @@ struct B final : public A {
         return 'B';
     }
 };
-
+int B::num_b = 0;
 int main(int argc, char **argv) {
     auto a = new A;
     auto b = new B;
-    ASSERT(A::num_a == ?, "Fill in the correct value for A::num_a");
-    ASSERT(B::num_b == ?, "Fill in the correct value for B::num_b");
-    ASSERT(a->name() == '?', "Fill in the correct value for a->name()");
-    ASSERT(b->name() == '?', "Fill in the correct value for b->name()");
+    // 因为创建了两个对象:
+    // 1. auto a = new A; 创建了一个A对象，A::num_a++
+    // 2. auto b = new B; 创建了一个B对象，由于B继承自A，也会调用A的构造函数，A::num_a++
+    // 所以此时 A::num_a == 2
+    ASSERT(A::num_a == 2, "Fill in the correct value for A::num_a");
+    ASSERT(B::num_b == 1, "Fill in the correct value for B::num_b");
+    ASSERT(a->name() == 'A', "Fill in the correct value for a->name()");
+    ASSERT(b->name() == 'B', "Fill in the correct value for b->name()");
 
     delete a;
     delete b;
@@ -48,13 +57,13 @@ int main(int argc, char **argv) {
     ASSERT(B::num_b == 0, "Every B was destroyed");
 
     A *ab = new B;// 派生类指针可以随意转换为基类指针
-    ASSERT(A::num_a == ?, "Fill in the correct value for A::num_a");
-    ASSERT(B::num_b == ?, "Fill in the correct value for B::num_b");
-    ASSERT(ab->name() == '?', "Fill in the correct value for ab->name()");
+    ASSERT(A::num_a == 1, "Fill in the correct value for A::num_a");
+    ASSERT(B::num_b == 1, "Fill in the correct value for B::num_b");
+    ASSERT(ab->name() == 'B', "Fill in the correct value for ab->name()");
 
     // TODO: 基类指针无法随意转换为派生类指针，补全正确的转换语句
-    B &bb = *ab;
-    ASSERT(bb.name() == '?', "Fill in the correct value for bb->name()");
+    B &bb = dynamic_cast<B&>(*ab);
+    ASSERT(bb.name() == 'B', "Fill in the correct value for bb->name()");
 
     // TODO: ---- 以下代码不要修改，通过改正类定义解决编译问题 ----
     delete ab;// 通过指针可以删除指向的对象，即使是多态对象
